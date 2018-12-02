7add526b41f33c202e393ee160bcefc54a10723e
Frikk <frikk.fossan@acando.no>
2018-12-02 12:32:46 +0100

    compiled typescript

--- simple-calculator/index.js
+++ simple-calculator/index.js
@@ -4,7 +4,8 @@ exports.index = function (context, req) {
     if (req.query.first && req.query.second) {
         var first = Number(req.query.first);
         var second = Number(req.query.second);
-        var result = first + second;
+        var third = Number(req.query.third);
+        var result = first + second + third;
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "" + result
